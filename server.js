b3759e9ef608a39b8831f70010dfa89a4d1c65a8
n8m <name.xru@gmail.com>
2014-02-18 00:17:31 +0300

    restructure

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
 var express = require('express');
+var path = require('path');
 var mongoose = require('mongoose');
 var textSearch = require('mongoose-text-search');
 
 var app = express();
 
-//
-//var postSchema = mongoose.Schema({
-//    title: String,
-//    urlTitle: String,
-//    shortContent: String,
-//    content: String,
-//    tags: [String],
-//    date: String,
-//    author: String
-//})
-//
-//postSchema.plugin(textSearch);
-//postSchema.index({ title: 'text', content: 'text', author: 'text', tags: "text" });
-//
-//var Post = mongoose.model('Post', postSchema);
 
-//mongoose.connect('mongodb://localhost/angudb', function () {
-//});
+var postSchema = mongoose.Schema({
+    title: String,
+    urlTitle: String,
+    shortContent: String,
+    content: String,
+    tags: [String],
+    date: String,
+    author: String
+})
+
+postSchema.plugin(textSearch);
+postSchema.index({ title: 'text', content: 'text', author: 'text', tags: "text" });
+
+var Post = mongoose.model('Post', postSchema);
+
+mongoose.connect('mongodb://localhost/angudb', function () {
+});
 
 app.use(express.favicon()); // отдаем стандартную фавиконку, можем здесь же свою задать
 app.use(express.logger('dev')); // выводим все запросы со статусами в консоль
 app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
 app.use(express.methodOverride()); // поддержка put и delete
 app.use(app.router); // модуль для простого задания обработчиков путей
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(path.join(__dirname, "app"))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
 
 
 app.all('*', function (req, res, next) {
