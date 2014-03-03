778daf02bca2da336f565bf96e091df2f2366dac
n8m <name.xru@gmail.com>
2014-03-04 00:19:20 +0300

    conf server routes

--- server.js
+++ server.js
@@ -235,8 +235,7 @@ app.get('/api/tags', function (req, res) {
 })
 
 app.get("*", function (req, res, next) {
-    console.log(__dirname);
-    res.sendfile(__dirname + '/app/index.html');
+    res.sendfile('/app/index.html');
 });
 
 app.listen(process.env.PORT || 1337, function () {
