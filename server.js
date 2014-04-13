9df81cfbcbba266af490bfd7394528ae1ed55e4a
n8m <name.xru@gmail.com>
2014-04-13 21:33:38 +0300

    prerender

--- server.js
+++ server.js
@@ -91,6 +91,9 @@ app.use(express.session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(app.router); // модуль для простого задания обработчиков путей
+app.use(require('prerender-node').set('prerenderToken', 'Tve6yMKDsJE20UEo8ANx'));//для prerender -> для SEO
+
+
 
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "meangu.ru");
